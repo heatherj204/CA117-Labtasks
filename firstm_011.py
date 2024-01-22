4664eb12b4a58d108b4fd17089229191d140a1eb
heatherj204 <heather.s.e.johnston@gmail.com>
2024-01-22 12:48:04 +0000

    firstm - not working

--- firstm_011.py
+++ firstm_011.py
@@ -2,3 +2,13 @@
 
 import sys
 
+for line in sys.stdin:
+    line = line.strip()
+    i = 0
+    find = True
+    while i < len(line) and find == True:
+        if line[i] == 'm':
+            line = line[i].replace(line[i], 'M', 1)
+            find = False
+        i = i + 1
+    print(line)
