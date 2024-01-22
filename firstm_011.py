5c141aaf3b09b0b5c7434138f8967cdf5981316b
heatherj204 <heather.s.e.johnston@gmail.com>
2024-01-22 22:01:13 +0000

    firstm done

--- firstm_011.py
+++ firstm_011.py
@@ -1,14 +1,7 @@
 #!/usr/bin/env python3
 
 import sys
-
 for line in sys.stdin:
     line = line.strip()
-    i = 0
-    find = True
-    while i < len(line) and find == True:
-        if line[i] == 'm':
-            line = line[i].replace(line[i], 'M', 1)
-            find = False
-        i = i + 1
+    line = line.replace('m', 'M', 1)
     print(line)
