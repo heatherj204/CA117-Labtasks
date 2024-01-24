82f87767d0818f95cf57a3dc93fd2149b9d06b32
heatherj204 <heather.s.e.johnston@gmail.com>
2024-01-24 00:29:39 +0000

    anagram_021-working

---
+++ week-2/anagram_021.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+import sys
+
+for line in sys.stdin:
+    line = line.split()
+    word = line[1].lower()
+    if len(line[0]) == len(word):
+        anag = True
+        for c in line[0]:
+            if anag:
+                c = c.lower()
+                anag = c in word
+                word = word.replace(c, "", 1)
+    else:
+        anag = False
+    print(anag)
