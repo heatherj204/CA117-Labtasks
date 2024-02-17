f6e5fd1c91612b4a1df3338578fdaede5be9cff4
heatherj204 <heather.s.e.johnston@gmail.com>
2024-02-17 23:09:16 +0000

    5.1 commit

---
+++ week-5/5.1/nightclub_051.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+import sys
+refuse = 0
+people = 0
+cap = sys.stdin.readline().strip()
+for line in sys.stdin:
+    line = line.strip()
+    line = line.split()
+    if line[0] == 'enter':
+        if people  + int(line[1]) <= int(cap):
+            people += int(line[1])
+        elif people  + int(line[1]) >= int(cap):
+            refuse += 1
+    elif line[0] == 'leave':
+        people = people - int(line[1])
+        
+print(refuse)
